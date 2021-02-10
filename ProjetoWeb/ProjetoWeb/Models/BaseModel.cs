ad31dfb2d8a76bba539307b085739f5a778ae574
OtonyelBrandao <otonyelbrandao@users.noreply.github.com>
2021-02-10 16:50:59 -0300

    Inicialização do Projeto

---
+++ ProjetoWeb/ProjetoWeb/Models/BaseModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace ProjetoWeb.Models
+{
+    [DataContract]
+    public class BaseModel : IComparable
+    {
+        [DataMember]
+        public int ID { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            if (!(obj is BaseModel outro)) return 1;
+
+            return ID.CompareTo(outro.ID);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is BaseModel model &&
+                   ID == model.ID;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(ID);
+        }
+    }
+}
